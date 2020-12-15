ed5c7e93238036df48f5965a6a9dcd018b3fe22d
Israel <israelfaustino.santos@gmail.com>
2020-12-15 14:32:44 -0300

    Added the option to choose the amount of numbers to rearrange

--- python/2.ascending.py
+++ python/2.ascending.py
@@ -1,13 +1,37 @@
-vec = [10,9,8,7,6,5,4,3,2,1]
-#print (vec[1])
+from random import seed
+from random import randint
 
-for a in range(0,len(vec),1):
-	#print (vec[a])
+# Random generator seed
+seed(1)
+# Array for the random numbers
+vec = []
+# Loop index
+a = 0
+
+temp = 0
+
+size = input("Insert the amount of number to rearange: ")
+size = int(size)
+
+print("\nThe random generated numbers are: ")
+
+for i in range(size):
+
+	# Generate random number up to 999
+	vec.append(randint(0,1000))
+	print(vec[i], end=" ")
+
+# rearrange the random numbers in ascending order
+for a in range(0,size,1):
 	for b in range(a + 1,len(vec),1):
 		#print (vec[a])
-		if vec[a] < vec[b]:
+		if vec[a] > vec[b]:
 			temp = vec[a]
 			vec[a] = vec[b]
 			vec[b] = temp
 
-print (vec)
+print("\n\nNow in ascending order: ")
+for a in vec:
+	print (a, end=" ")
+
+print("")
