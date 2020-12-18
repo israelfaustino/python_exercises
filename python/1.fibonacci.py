d2ff13e773893c5fec5f2d0248185edc69781397
Israel <israelfaustino.santos@gmail.com>
2020-12-18 07:08:44 -0300

    Added option to choose the amount of numbers to generate.

--- python/1.fibonacci.py
+++ python/1.fibonacci.py
@@ -1,8 +1,22 @@
 old = 0
 new = 0
 cur = 1
-while(cur < 100):
-	print(cur, end=" ")
+size = 100
+count = 0
+
+size = input("Insert the amount of numbers to be generated (0-1000): ")
+size = int(size)
+
+# Number limit
+if size > 1000:
+	print("Size cannot be bigger than 100")
+	size = 100
+
+# Main loop
+while(count < size):
+	count = count + 1
+	# Prints the generated number with padded index
+	print(str(count).rjust(4,'0'),". ", cur, end="\n", sep="")
 	new = old + cur
 	old = cur
 	cur = new
